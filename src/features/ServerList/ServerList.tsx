30ddedc3c34f52fa6071298c05f2d646c0553993
justheimsk <vonderheimsk@gmail.com>
2024-09-29 16:26:01 -0300

    feat: add ServerList feature

---
+++ src/features/ServerList/ServerList.tsx
@@ -0,0 +1,20 @@
+import ServerButton from './components/ServerButton/ServerButton';
+import './ServerList.scss';
+
+export default function ServerList() {
+  return (
+    <>
+      <div id="server-list">
+        <div className="server-list__panel server-list__panel--small">
+          <ServerButton />
+          <ServerButton />
+        </div>
+        <div className="server-list__panel">
+          {"h".repeat(20).split('').map(() => (
+            <ServerButton />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
