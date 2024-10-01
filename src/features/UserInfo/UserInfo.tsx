e9a0af183846450054844e408d2abac79a48b9d1
justheimsk <vonderheimsk@gmail.com>
2024-09-30 22:24:26 -0300

    feat: implement UserInfo component

--- src/features/UserInfo/UserInfo.tsx
+++ src/features/UserInfo/UserInfo.tsx
@@ -1,9 +1,22 @@
+import {FaHeadphones, FaMicrophone} from "react-icons/fa6";
 import "./UserInfo.scss";
+import {FaCog} from "react-icons/fa";
 
 export default function UserInfo() {
   return (
     <>
-      <div id="user-info"></div>
+      <div id="user-info">
+        <div id="user-info__avatar" />
+        <div id="user-info__infos">
+          <span>vonderheimsk</span>
+          <small>Online</small>
+        </div>
+        <div id="user-info__actions">
+          <i><FaMicrophone /></i>
+          <i><FaHeadphones /></i>
+          <i><FaCog /></i>
+        </div>
+      </div>
     </>
   )
 }
