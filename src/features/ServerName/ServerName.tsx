fa87d48dac1f7cbec8ef455777bb659d371cb9ff
justheimsk <henrique.mdimer@gmail.com>
2024-09-30 22:24:49 -0300

    feat: implement ServerName component

--- src/features/ServerName/ServerName.tsx
+++ src/features/ServerName/ServerName.tsx
@@ -1,7 +1,13 @@
+import {FaAngleDown} from "react-icons/fa6";
 import "./ServerName.scss";
 
 export default function ServerName() {
   return (
-    <div id="server-name"></div>
+    <div id="server-name">
+      <div>
+        <span>Name Placeholder</span>
+        <FaAngleDown />
+      </div>
+    </div>
   )
 }
