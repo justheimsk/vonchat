da7383da59b9293fc935b068865e702284c7e794
justheimsk <vonderheimsk@gmail.com>
2024-09-30 22:23:59 -0300

    refactor: minor changes

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -1,11 +1,11 @@
 import ServerList from '@features/ServerList/ServerList'
 import './home.scss'
-import ChannelList from '@/features/ChannelList/ChannelList'
-import ServerName from '@/features/ServerName/ServerName'
-import UserInfo from '@/features/UserInfo/UserInfo'
-import ChannelInfo from '@/features/ChannelInfo/ChannelInfo'
-import ChannelData from '@/features/ChannelData/ChannelData'
-import UserList from '@/features/UserList/UserList'
+import ChannelList from '@features/ChannelList/ChannelList'
+import ServerName from '@features/ServerName/ServerName'
+import UserInfo from '@features/UserInfo/UserInfo'
+import ChannelInfo from '@features/ChannelInfo/ChannelInfo'
+import ChannelData from '@features/ChannelData/ChannelData'
+import UserList from '@features/UserList/UserList'
 
 export default function App() {
   return (
