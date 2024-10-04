7d917f728a0689d2a1736c57f3a448b8ec0afb18
justheimsk <vonderheimsk@gmail.com>
2024-10-03 23:00:06 -0300

    feat(Input): add global components

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,15 @@
+import {FaSearch} from "react-icons/fa";
+import "./Input.scss";
+
+export interface InputProps extends React.HTMLProps<HTMLInputElement> {}
+
+export default function Input(props: InputProps) {
+  return (
+    <>
+      <div className="input-container">
+        <input {...props} />
+        <i><FaSearch /></i>
+      </div>
+    </>
+  )
+}
