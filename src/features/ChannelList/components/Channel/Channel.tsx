a82557585230e319fbefa82d9159806b57fa6f46
justheimsk <henrique.mdimer@gmail.com>
2024-10-03 23:00:54 -0300

    chore(font-size): minor changes

--- src/features/ChannelList/components/Channel/Channel.tsx
+++ src/features/ChannelList/components/Channel/Channel.tsx
@@ -1,14 +1,17 @@
 import {FaHashtag, FaUserPlus} from "react-icons/fa6";
 import "./Channel.scss";
 import {FaCog} from "react-icons/fa";
+import {useState} from "react";
 
 export function Channel() {
+  const [name, _] = useState('general-chat');
+
   return (
     <>
       <div className="channel">
         <div className="channel__infos">
           <i><FaHashtag /></i>
-          <span>General chat</span>
+          <span>{name}</span>
         </div>
         <div className="channel__actions">
           <i><FaUserPlus /></i>
