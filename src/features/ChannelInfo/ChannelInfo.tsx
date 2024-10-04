a3ad5a1d32de0169ebc7af7ccdfdc345eb822b1d
justheimsk <henrique.mdimer@gmail.com>
2024-10-03 23:00:29 -0300

    feat: add ChannelInfo component

--- src/features/ChannelInfo/ChannelInfo.tsx
+++ src/features/ChannelInfo/ChannelInfo.tsx
@@ -1,9 +1,24 @@
+import {FaBell, FaHashtag, FaInbox, FaUsers} from "react-icons/fa6";
 import "./ChannelInfo.scss";
+import {TiPin} from "react-icons/ti";
+import Input from "@/components/Input/Input";
 
 export default function ChannelInfo() {
   return (
     <>
-      <div id="channel-info"></div>
+      <div id="channel-info">
+        <div id="channel-info__infos">
+          <i><FaHashtag /></i>
+          <span>General</span>
+        </div>
+        <div id="channel-info__actions">
+          <i><FaBell /></i>
+          <i><TiPin /></i>
+          <i><FaUsers /></i>
+          <i><FaInbox /></i>
+          <Input placeholder="Search" />
+        </div>
+      </div>
     </>
   )
 }
