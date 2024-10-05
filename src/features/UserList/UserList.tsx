324eea58f527216b8803a27f5ab35ac82516ec9f
justheimsk <vonderheimsk@gmail.com>
2024-10-05 12:17:28 -0300

    feat: add User component

--- src/features/UserList/UserList.tsx
+++ src/features/UserList/UserList.tsx
@@ -1,9 +1,14 @@
+import {User} from "./components/User/User";
 import "./UserList.scss";
 
 export default function UserList() {
   return (
     <>
-      <div id="user-list"></div>
+      <div id="user-list">
+        {"h".repeat(20).split('').map(() => (
+          <User />
+        ))}
+      </div>
     </>
   )
 }
---
+++ src/features/UserList/components/User/User.tsx
@@ -0,0 +1,15 @@
+import "./User.scss";
+
+export function User() {
+  return (
+    <>
+      <div className="user">
+        <div className="user__avatar"></div>
+        <div className="user__infos">
+          <span>vonderheimsk</span>
+          <small>Online</small>
+        </div>
+      </div>
+    </>
+  )
+}
