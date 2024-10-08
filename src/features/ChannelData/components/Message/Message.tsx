7970c6216398c8f33ca6c58d5001f38cf2b15e93
justheimsk <vonderheimsk@gmail.com>
2024-10-08 11:20:53 -0300

    feat: add Message component

---
+++ src/features/ChannelData/components/Message/Message.tsx
@@ -0,0 +1,18 @@
+import "./Message.scss";
+
+export default function Message() {
+  return (
+    <>
+      <div className="message">
+        <div className="message__avatar" />
+        <div className="message__data">
+          <div className="message__data__infos">
+            <span>vonderheimsk</span>
+            <small>Today at 2:51 pm</small>
+          </div>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nobis voluptatum repellendus similique quidem blanditiis deleniti illum ratione voluptas. Vero in assumenda eveniet laborum ea quis fuga corrupti expedita atque!</span>
+        </div>
+      </div>
+    </>
+  )
+}
