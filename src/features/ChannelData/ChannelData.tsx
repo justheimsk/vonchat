e4a10eb4799ca159abc862fac9c03fcd92520c02
justheimsk <henrique.mdimer@gmail.com>
2024-10-08 11:20:53 -0300

    feat: add Message component

--- src/features/ChannelData/ChannelData.tsx
+++ src/features/ChannelData/ChannelData.tsx
@@ -1,10 +1,16 @@
 import "./ChannelData.scss";
 import ChatInput from "./components/ChatInput/ChatInput";
+import Message from "./components/Message/Message";
 
 export default function ChannelData() {
   return (
     <>
       <div id="channel-data">
+        <div id="channel-data__messages">
+          {"h".repeat(20).split('').map(() => (
+            <Message />
+          ))}
+        </div>
         <ChatInput />
       </div>
     </>
