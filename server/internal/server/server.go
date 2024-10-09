f6e13a8a0a9217188ed9ece57d97302c49047372
justheimsk <henrique.mdimer@gmail.com>
2024-10-09 18:51:44 -0300

    refactor: improve code architecture

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/justheimsk/vonchat/server/internal/services/healthCheck"
+	controllers "github.com/justheimsk/vonchat/server/internal/services/healthCheck/controller"
+	handlers "github.com/justheimsk/vonchat/server/internal/services/healthCheck/http"
+	repositories "github.com/justheimsk/vonchat/server/internal/services/healthCheck/repository"
 )
 
 type Server struct {
@@ -25,8 +27,16 @@ func (self *Server) Init() {
 	self.logger.Println("Starting HTTP server...")
 	mainRouter := chi.NewRouter()
 
-	healthCheck := healthCheckService.New(self.logger, self.db)
-	mainRouter.Route("/", healthCheck.Handler.Load)
+	// Repository
+	healthRepo := repositories.NewHealthRepo(self.db)
+
+	// Controllers
+	healthController := controllers.NewHealthController(healthRepo)
+
+	// Handlers
+	healthCheck := handlers.NewHealthHandler(healthController)
+
+	mainRouter.Route("/", healthCheck.Load)
 
 	log.Printf("Serving HTTP in port: %d\n", PORT)
 	if err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", PORT), mainRouter); err != nil {
--- server/internal/services/healthCheck/controller.go
+++ server/internal/services/healthCheck/controller.go
@@ -1,29 +1,7 @@
 package healthCheckService
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
+import "net/http"
 
-type healthCheckController struct {
-	logger *log.Logger
-	repo   *healthCheckRepo
-}
-
-func NewController(logger *log.Logger, repo *healthCheckRepo) *healthCheckController {
-	return &healthCheckController{
-		logger,
-		repo,
-	}
-}
-
-func (self *healthCheckController) CheckHealth(w http.ResponseWriter, r *http.Request) {
-	ping, err := self.repo.GetPing()
-	if err != nil {
-		fmt.Fprintf(w, "Failed to get database ping: %s", err)
-		return
-	}
-
-	fmt.Fprintf(w, "All systems operational, database ping = %dms", ping.Milliseconds())
+type Controller interface {
+	CheckHealth(w http.ResponseWriter, r *http.Request)
 }
