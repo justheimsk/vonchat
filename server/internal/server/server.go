4bb718f8484f548dcd3dd29ff5d9c24670f57cb3
justheimsk <henrique.mdimer@gmail.com>
2024-10-09 17:26:22 -0300

    style: format code

--- server/internal/server/server.go
+++ server/internal/server/server.go
@@ -10,22 +10,22 @@ import (
 )
 
 type Server struct {
-  db *sql.DB
-  logger *log.Logger
+	db     *sql.DB
+	logger *log.Logger
 }
 
-func NewServer(db *sql.DB, logger *log.Logger) (*Server) {
-  return &Server{ db, logger }
+func NewServer(db *sql.DB, logger *log.Logger) *Server {
+	return &Server{db, logger}
 }
 
 func (self *Server) Init() {
-  self.logger.Println("Starting HTTP server...")
-  
-  mainRouter := chi.NewRouter()
-  healthCheckService := services.NewHealthCheckService(self.logger)
-  mainRouter.Route("/", healthCheckService.Handler.Load)
+	self.logger.Println("Starting HTTP server...")
 
-  if err := http.ListenAndServe("0.0.0.0:8080", mainRouter); err != nil {
-    log.Fatalf("Failed to start HTTP server: %s", err)
-  }
+	mainRouter := chi.NewRouter()
+	healthCheckService := services.NewHealthCheckService(self.logger)
+	mainRouter.Route("/", healthCheckService.Handler.Load)
+
+	if err := http.ListenAndServe("0.0.0.0:8080", mainRouter); err != nil {
+		log.Fatalf("Failed to start HTTP server: %s", err)
+	}
 }
