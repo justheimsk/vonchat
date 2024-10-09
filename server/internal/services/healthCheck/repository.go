cd53e18054dea71c391fa911cef624ca5269bcce
justheimsk <vonderheimsk@gmail.com>
2024-10-09 18:21:36 -0300

    feat: add repositories

---
+++ server/internal/services/healthCheck/repository.go
@@ -0,0 +1,27 @@
+package healthCheckService
+
+import (
+	"database/sql"
+	"time"
+)
+
+type healthCheckRepo struct {
+	db *sql.DB
+}
+
+func NewRepo(db *sql.DB) *healthCheckRepo {
+	return &healthCheckRepo{
+		db,
+	}
+}
+
+func (self *healthCheckRepo) GetPing() (time.Duration, error) {
+	start := time.Now()
+  
+  err := self.db.Ping()
+  if err != nil {
+    return 0, err
+  }
+
+	return time.Since(start), nil
+}
