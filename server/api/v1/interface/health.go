3bdef90b4901d8d727f7108b1717fafd3570cfd7
justheimsk <henrique.mdimer@gmail.com>
2024-10-11 12:00:09 -0300

    refactor: merge interfaces

--- server/api/v1/interface/health.go
+++ server/api/v1/interface/health.go
@@ -1,7 +1,14 @@
 package interfaces
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
 
 type HealthController interface {
 	CheckHealth(w http.ResponseWriter, r *http.Request)
+}
+
+type HealthRepository interface {
+	GetPing() (time.Duration, error)
 }
