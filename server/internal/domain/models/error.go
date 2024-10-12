90255588ce8f37e34f6c345c17ca7dc20f2f31bc
justheimsk <vonderheimsk@gmail.com>
2024-10-12 13:37:52 -0300

    chore: add custom standardized errors

---
+++ server/internal/domain/models/error.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type CustomError struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code"`
+}
+
+const (
+	NotFoundErrorCode = iota
+	DuplicateErrorCode
+	InternalErrorCode
+)
+
+var (
+	ErrNotFound   = NewCustomError(NotFoundErrorCode, "Requested resource not found.")
+	ErrDuplicate  = NewCustomError(DuplicateErrorCode, "Duplicate entry.")
+	InternalError = NewCustomError(InternalErrorCode, "Internal server error.")
+)
+
+func NewCustomError(statuscode int, message string) *CustomError {
+	return &CustomError{
+		message,
+		statuscode,
+	}
+}
+
+func (self *CustomError) Error() string {
+	return self.Message
+}
+
+func (self *CustomError) ToJSON() ([]byte, error) {
+	return json.Marshal(self)
+}
+
+func (self *CustomError) ToHttpStatusCode() *CustomError {
+	switch self.StatusCode {
+	case NotFoundErrorCode:
+		return NewCustomError(http.StatusNotFound, self.Message)
+	case DuplicateErrorCode:
+		return NewCustomError(http.StatusConflict, self.Message)
+	case InternalErrorCode:
+		return NewCustomError(http.StatusInternalServerError, self.Message)
+	default:
+		return self
+	}
+}
