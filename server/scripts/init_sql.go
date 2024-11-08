d2d4e0c4d66c8bd8875813ce2157c8ba0692d040
Henrique <henrique.mdimer@gmail.com>
2024-11-08 15:21:52 -0300

    chore: add PostgreSQL init script

---
+++ server/scripts/init_sql.go
@@ -0,0 +1,14 @@
+package scripts
+
+func GetPQInitScript() string {
+  return `CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            username varchar(50) NOT NULL,
+            email varchar(255) UNIQUE NOT NULL,
+            password varchar(999) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            CONSTRAINT not_empty_username CHECK (username <> ''),
+            CONSTRAINT not_empty_email CHECK (email <> '')
+          )
+  `
+}
