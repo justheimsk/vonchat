bb5fd5d810fc71f61783cf5c0abe2d57c13b66af
justheimsk <vonderheimsk@gmail.com>
2024-11-09 13:41:25 -0300

    chore(scripts): add sqlite init script

--- server/scripts/init_sql.go
+++ server/scripts/init_sql.go
@@ -12,3 +12,16 @@ func GetPGInitScript() string {
           )
   `
 }
+
+func GetSQLiteInitScript() string {
+  return `CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            CONSTRAINT not_empty_username CHECK (username <> ''),
+            CONSTRAINT not_empty_email CHECK (email <> '')
+          )
+  `
+}
