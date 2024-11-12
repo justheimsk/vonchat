5a55bab6836f0c1a4278f0e7bfdd36fd9e2f8686
justheimsk <vonderheimsk@gmail.com>
2024-11-12 17:17:52 -0300

    feat(pgsql): add PostgreSQL user repository implementation

---
+++ server/internal/infra/persistence/repository/pgsql/user_repo_impl.go
@@ -0,0 +1,29 @@
+package pgsql
+
+import (
+	"database/sql"
+
+	"github.com/justheimsk/vonchat/server/internal/domain/models"
+)
+
+type userRepository struct {
+  db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *userRepository {
+  return &userRepository{
+    db,
+  }
+}
+
+func (self *userRepository) GetUserById(id string) (*models.User, error) {
+  user := &models.User{}
+
+  str := "SELECT * FROM users WHERE id=$1"
+  err := self.db.QueryRow(str, id).Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt)
+  if err != nil {
+    return nil, err
+  }
+
+  return user, nil
+}
