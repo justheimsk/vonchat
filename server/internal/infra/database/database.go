428ad40b78fea6a2a4d53471b1691bf85265e2a1
Henrique <henrique.mdimer@gmail.com>
2024-11-18 11:41:02 -0300

    chore(database): add database factory for driver creation

--- server/internal/infra/database/database.go
+++ server/internal/infra/database/database.go
@@ -1,10 +1,26 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/justheimsk/vonchat/server/internal/domain/models"
+	"github.com/justheimsk/vonchat/server/internal/infra/config"
+)
 
 type DatabaseDriver interface {
   Open() error
   Close()
   GetDB() *sql.DB
   GetName() string
+}
+
+func NewDatabaseDriver(driverName string, config *config.Config, logger models.Logger) DatabaseDriver {
+  switch (driverName) {
+  case "POSTGRES":
+    return NewPostgresDatabaseDriver(config, logger)
+  case "SQLITE":
+    return NewSQLiteDatabaseDriver(config, logger)
+  }
+
+  return nil
 }
