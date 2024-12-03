59fc610a0bc040eb908b0fb90f71032af2337882
Henrique <henrique.mdimer@gmail.com>
2024-12-03 20:33:31 -0300

    feat(commands): add basic command list renderer

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,5 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
+import uiSlice from './slices/ui'
 
-export default configureStore({
-  reducer: {}
+const store = configureStore({
+  reducer: {
+    uiSlice: uiSlice
+  }
 })
+
+export default store;
+
+// Get the type of our store variable
+export type AppStore = typeof store
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = AppStore['dispatch']
