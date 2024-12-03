c78425231a19c8acae8ae1f2f36670f1a270a239
justheimsk <vonderheimsk@gmail.com>
2024-12-03 20:33:04 -0300

    feat(modal): add modal component

---
+++ client/src/components/Modal/Modal.tsx
@@ -0,0 +1,18 @@
+import "./Modal.scss";
+
+export interface ModalProps {
+  title: string;
+  active: boolean;
+  onClose: () => void;
+}
+
+export function Modal(props: ModalProps) {
+  return (
+    <>
+      <div onClick={() => props.onClose?.()} className={`modal__overlay ${props.active ? 'modal__overlay--active' : ''}`} />
+      <div className={`modal ${props.active ? 'modal--active' : ''}`}>
+        <h3>{props.title}</h3>
+      </div>
+    </>
+  )
+}
