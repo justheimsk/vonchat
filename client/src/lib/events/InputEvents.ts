520a8a28bcbb77ed6272aac6a67d58b3c53650ef
Henrique <henrique.mdimer@gmail.com>
2024-12-06 09:35:18 -0300

    refactor(events): remove useless event

--- client/src/lib/events/InputEvents.ts
+++ client/src/lib/events/InputEvents.ts
@@ -3,5 +3,4 @@ import {Observable} from "../core/Observable";
 export class InputEvents {
   public clearChatInput = new Observable<null>;
   public setChatInput = new Observable<string>;
-  public appendChatInput = new Observable<string>;
 }
