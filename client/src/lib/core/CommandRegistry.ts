96911666ea2e35eadb68feae50302396a5749f9f
Henrique <henrique.mdimer@gmail.com>
2024-12-06 09:35:39 -0300

    feat(commands): add support for command arguments

--- client/src/lib/core/CommandRegistry.ts
+++ client/src/lib/core/CommandRegistry.ts
@@ -1,17 +1,39 @@
 import store from "@/store/store";
-import Command from "./Command";
+import Command, {type Arg} from "./Command";
 import {registerCommand} from "@/store/slices/commandRegistry";
 
+export type CommandCallback = (ctx: RecvContext) => void;
+
+export interface RecvArg {
+  name: string;
+  value: unknown;
+}
+export interface RecvContext {
+  args: Map<string, RecvArg>
+}
+
 export default class CommandRegistry {
-  public register(name: string, description: string, execv: (...args: unknown[]) => void) {
-    const command = new Command(name, description, execv);
+  public register(name: string, description: string, args: Arg[], execv: CommandCallback) {
+    const command = new Command(name, description, args, execv);
     store.dispatch(registerCommand(command));
   }
 
-  public exec(name: string) {
-    const cmd = store.getState().commandRegistry.commands.find((cmd) => cmd.name === name);
+  public fetch(name: string) {
+    return this.getState().commands.find((cmd) => cmd.name === name);
+  }
+
+  public exec(name: string, args: RecvArg[]) {
+    const cmd = this.getState().commands.find((cmd) => cmd.name === name);
+    const ctx: RecvContext = {
+      args: new Map()
+    }
+
+    for(const arg of args) {
+      ctx.args.set(arg.name, arg);
+    }
+
     if(cmd) {
-      cmd.execv();
+      cmd.execv(ctx);
     }
   }
 
